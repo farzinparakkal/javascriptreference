60c00f22d9c3a6f4e77dfc3e7b8dcef4b776d49e
farzin <farzinparakkal123@gmail.com>
2024-08-17 04:25:34 +0530

    friday commit

--- script.js
+++ script.js
@@ -1,122 +1,119 @@
 
+//q1
 {
-{
-    function prnum(){
-        let i
-        for(i=1;i<=10;i++)
-        console.log(i)
-    }
-    prnum()
-}
-}
-
-
-
-
-{
-function sumn(){
-    let i,sum=0
-    for(i=1;i<=10;i++)
-        sum = sum+i
-    return sum
-}
-q = sumn()
-console.log("Sum : ",q)
-}
-
-
-
-
-{
-function evep(){
-    let i
-    for(i=1;i<=20;i++)
-    {
-        if(i%2==0)
-            console.log(i)
-    }
-}
-evep()
-}
-
-
-
-
-{
-function cvow(str){
-
-c=0
-strlen = str.length
-for(let i=0;i<=strlen;i++)
-    {
-    if(str[i]=="a" || str[i]=='A' || str[i]=='e' ||str[i]=='E' ||str[i]=='i' ||str[i]=='I' ||str[i]=='o' ||str[i]=='O' ||str[i]=='u' ||str[i]=='U')
-        c=c+1
-    }
-    console.log("string = ",str)
-    return c
-}
-str = "hi how are you"
-c=cvow(str)
-console.log("No of vowels = ",c)
-}
-
-
-
-{
-function prime(){
-    let k=83,c,i
-    for(i=2;i<=10;i++){
-        if(k==i)
-            continue
-        else if(k%i==0){
-            c=1
-            console.log("It is not a prime number !")
-            break
+    function fsum(arr){
+        let i,s=0
+        for(i=0;i<5;i++)
+        {
+            s=s+arr[i]
         }
-        else if(k%i!=0){
-            c=0
-        }
+        return s
     }
-    if(c==0)
-    {
-        console.log("It is a prime number !")
-    }
-}
-prime()
+
+    arr=[10,20,30,40,50]
+    os=fsum(arr)
+    console.log("Sum : ",os)
 }
 
-
-
-
-
-
-
+//q2
 {
-function pstar(){
-    for (let i = 1; i <= 5; i++) {
-        let p = '';
-        for (let j = 1; j <= i; j++) {
-            p = p + j;
+    function fsum(arr){
+        let i,s=0
+        for(i=0;i<5;i++)
+        {
+            s=s+arr[i]
         }
-        console.log(p);
+        return s
     }
+    function Avg(os,n){
+        avg=os/n
+        return avg
+    }
+
+    arr=[10,20,30,40,50]
+    os=fsum(arr)
+    avg=Avg(os,5)
+    console.log("Average : ",avg)
 }
-pstar()
-}
 
-
-
-
+//q3
 {
-function starp(){
-    let i,j,str=``
-    for(i=5;i>0;i--){
-        for(j=1;j<=i;j++)
-            str = str + "*"
-        str = str + "\n"
+    function minmax(arr){
+        let i,m,n
+        m=arr[0]
+        n=arr[0]
+        for(i=0;i<5;i++)
+        {
+            if(arr[i]>m)
+                m=arr[i]
+            else if(arr[i]<n)
+                n=arr[i]
+        }
+        console.log("Max : ",m)
+        console.log("Min : ",n)
+
     }
-    return str
+
+    arr=[10,20,30,40,50]
+    minmax(arr)
 }
-strm = starp()
-console.log(strm)
+
+//q4
+{
+    function abs(n){
+        pn=Math.abs(n)
+        console.log("Possitive num is :",pn)
+    }
+    n=-10
+    abs(n)
+    m=20
+    abs(m)
+}
+
+//q5
+{
+    function fsum(arr){
+        let i,s=0
+        for(i=0;i<5;i++)
+        {
+            if((arr[i]%2)==0)
+                s=s+arr[i]
+        }
+        return s
+    }
+
+    arr=[10,19,30,41,50]
+    os=fsum(arr)
+    console.log('array : ',arr)
+    console.log("even sum = ",os)
+}
+
+//q6
+{
+    function fsum(arr){
+        let i,s=0
+        for(i=0;i<5;i++)
+        {
+            if((arr[i]%2)!=0)
+                s=s+arr[i]
+        }
+        return s
+    }
+
+    arr=[10,19,30,41,50]
+    os=fsum(arr)
+    console.log('array : ',arr)
+    console.log("odd sum = ",os)
+}
+
+//factorial
+{
+    function fact(n){
+        if(n==0 ||  n==1)
+            return 1
+        return n*fact(n-1)
+    }
+    let a =4
+    re=fact(a)
+    console.log("factorial : ",re)
 }
